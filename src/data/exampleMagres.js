92edb252c7eff04fda0a7f8336f8d154c92b9f12
Kane Shenton <kane.shenton@stfc.ac.uk>
2024-10-18 15:19:22 +0100

    Provide a few more example files

--- src/data/exampleMagres.js
+++ src/data/exampleMagres.js
@@ -3,6 +3,9 @@
 // Convenience file to store the test input files as variables
 let exampleFiles = {};
 
-exampleFiles['ethanol'] = 'ethanol.magres';
+exampleFiles['Ethanol'] = 'ethanol.magres';
+exampleFiles['Alanine'] = 'alanine.magres';
+exampleFiles['EDIZUM']  = 'EDIZUM.magres';
+exampleFiles['Al2O3']   = 'Al2O3.magres';
 
 export { exampleFiles }
